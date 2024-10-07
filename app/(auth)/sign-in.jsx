fb3833f2178c5b39797b1551d9b74990d8467119
ilyass <anidailyass@gmail.com>
2024-10-07 14:23:31 +0100

    Add submit button in the sign in page,

--- app/(auth)/sign-in.jsx
+++ app/(auth)/sign-in.jsx
@@ -3,13 +3,16 @@ import React, { useState } from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import { images } from '../../constants'
 import FormField from '../../components/FormField'
+import CustomButton from '../../components/CustomButton'
+import { Link } from 'expo-router'
 
 const SignIn = () => {
 
   const [form, setForm] = useState({
     email: "",
     password: "",
-  })
+  });
+  const [isSubmiting, setIsSubmiting] = useState(false);
 
   return (
     <SafeAreaView className="bg-primary h-full">
@@ -35,14 +38,31 @@ const SignIn = () => {
           <FormField
             title="Password"
             placeholder="Enter your password"
-            value={form.email}
+            value={form.password}
             handleChangeText={(e) => setForm({
               ...form, password: e
             })}
             otherStyles="mt-7"
             secure
           />
+
+
+        <CustomButton
+          title="Sign In"
+          containerStyles="mt-8"
+          isLoading={isSubmiting}
+        />
+
+        <View className="flex-row justify-center items-center gap-2 pt-5">
+          <Text className="text-lg font-pregular text-gray-100 ">
+            Don't have account?
+          </Text>
+          <Link href="sign-up" className='text-lg font-psemibold text-secondary'>
+            Sign Up
+          </Link>
         </View>
+        </View>
+
       </ScrollView>
     </SafeAreaView>
   )
--- app/_layout.jsx
+++ app/_layout.jsx
@@ -26,8 +26,8 @@ const RootLayout = () => {
 
   return (
     <Stack>
-      <Stack.Screen name='(tabs)' options={{title:"tabs"}}/>
       <Stack.Screen name='index' options={{headerShown:false}}/>
+      <Stack.Screen name='(auth)' options={{headerShown:false}}/>
     </Stack>
   )
 }
